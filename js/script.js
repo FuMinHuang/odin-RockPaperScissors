78ec02d2acc18328460d9927164ab1ce1ac3f48d
ILoveEgg0001 <98178807+ILoveEgg0001@users.noreply.github.com>
2023-06-21 21:04:22 -0700

    Completed the program
    - Add the game() function.
    - Move userScore and computerScore out as global variable.

--- js/script.js
+++ js/script.js
@@ -5,40 +5,56 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-//Get user input
+// Get user input
 function getPlayerChoice() {
     const plyaserInput = prompt("Please enter either Rock, Paper, or Scissors (case-insensitive)", )
     const playserInputCapitalized = plyaserInput.charAt(0).toUpperCase() + plyaserInput.slice(1).toLowerCase();
     return playserInputCapitalized;
 }
 
-//RPS compare rule
+// RPS compare rule
 const RPS = {
     Rock: {weakTo: "Paper", strongTo: "Scissors"},
     Paper: {weakTo: "Scissors", strongTo: "Rock"},
     Scissors: {weakTo: "Rock", strongTo: "Paper"}
 }
 
-//Play single round
+let userScore = 0;
+let computerScore = 0;
+
+// Play single round
 function playRound(playserSelection, computerSelection) {
+    // Display choices
     console.log("Your choice is " + playserSelection);
     console.log("Computer choice is " + computerSelection);
+
+    // Comparasion
     if (RPS[playserSelection].strongTo === computerSelection) {
-        console.log(`You Win! ${playserSelection} beats ${computerSelection}`);
+        console.log(`${playserSelection} beats ${computerSelection}`);
+        console.log(`You Win! Your current score is ${userScore +=1}. Computer 's current score is ${computerScore}`);
+        // userScore += 1;
     }else if (RPS[playserSelection].weakTo === computerSelection) {
-        console.log(`You Lose! ${computerSelection} beats ${playserSelection}`);
+        console.log(`${computerSelection} beats ${playserSelection}`);
+        console.log(`You Lose! Your current score is ${userScore}. Computer 's current score is ${computerScore += 1}`);
+        // computerScore += 1;
     }else {
         console.log("Draw!");
+        console.log(`You Lose! Your current score is ${userScore}. Computer 's current score is ${computerScore}`);
     }
+    return {userScore, computerScore};
 }
 
-//Play 5 times
-function game() {
-    playRound(getPlayerChoice(), getComputerChoice());
-    playRound(getPlayerChoice(), getComputerChoice());
-    playRound(getPlayerChoice(), getComputerChoice());
-    playRound(getPlayerChoice(), getComputerChoice());
-    playRound(getPlayerChoice(), getComputerChoice());
+
+// Play 5 times
+function game(n) {
+    for (let i = 0; i < n; i++) {
+        (playRound(getPlayerChoice(), getComputerChoice()));
+    }
+    if (userScore > computerScore) {
+        console.log("You are winner!");
+    }else if (userScore < computerScore) {
+        console.log("Computer is winner!");
+    }else console.log("Draw Game!");
 }
 
-game()
+game(5);
